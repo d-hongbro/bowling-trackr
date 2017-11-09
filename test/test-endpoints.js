04ba8e9bf76cdea2ecba0e3513e550b0501b3167
David Hong <davidhong342@gmail.com>
2017-11-08 17:52:27 -0800

    travis and heroku deployment

---
+++ test/test-endpoints.js
@@ -0,0 +1,32 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+const {app} = require('../server');
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+const {TEST_DATABASE_URL} = require('../config');
+const {app, runServer, closeServer} = require('../server');
+
+describe('Bowling Trackr API Resource', function() {
+
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  after(function() {
+    return closeServer();
+  })
+
+	describe('GET Public index endpoint', function() {
+		it('should return 200 along with index.html', function() {
+			return chai.request(app)
+				.get('')
+				.then(function(res) {
+					res.should.have.status(200);
+				});
+		});
+	});
+});
